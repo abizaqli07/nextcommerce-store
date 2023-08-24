3d07472ee8c5a04697da8f529e677bf79e4fa8f1
abikhoir07 <abikhoir42@gmail.com>
2023-08-24 21:39:56 +0700

    small changes on category page

--- src/app/(routes)/category/[categoryId]/page.tsx
+++ src/app/(routes)/category/[categoryId]/page.tsx
@@ -1,6 +1,11 @@
 export const revalidate = 0;
 
+import getCategory from "@/actions/get_category";
+import getColors from "@/actions/get_colors";
 import getProducts from "@/actions/get_products";
+import getSizes from "@/actions/get_sizes";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
 import { FC } from "react";
 
 interface CategoryPageProps {
@@ -23,8 +28,26 @@ const CategoryPage: FC<CategoryPageProps> = async ({
     sizeId: searchParams.sizeId
   });
 
+  const sizes = await getSizes();
+  const colors = await getColors();
+  const category = await getCategory(params.categoryId)
+
   return (
-    <div>CategoryPage</div>
+    <div className=" bg-white">
+      <Container>
+        <Billboard
+          data={category.billboard}
+        />
+        <div className=" px-4 sm:px-6 lg:px-8 pb-24">
+          <div className=" lg:grid lg:grid-cols-5 lg:gap-x-8">
+            {/* Add Mobile filters */}
+            <div className=" hidden lg:block">
+              {/* todo : Filter */}
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
